b1666bc4e04b6d3a9743d7d8b8b836a292b49e22
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2015-03-19 17:55:48 +0100

    AngularJS init

--- public_html/js/app.js
+++ public_html/js/app.js
@@ -1,7 +1,12 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+//Déclarayion de notre application AngularJS avec les modules nécessaires
+var app = angular.module('ParkingNantes', ['ngRoute']);
 
+/* Configuration des routes*/
+app.config(['$routeProvider',function ($routeProvider) {
+  $routeProvider.when('/home', { 
+    templateUrl: 'view/viewHome.html',
+    controller:'HomeController'  
+  })  
+  .otherwise({redirectTo : '/home'});
+}]);
 
---
+++ public_html/js/controller/HomeController.js
@@ -0,0 +1,10 @@
+/*
+* Controller auth
+* Gestion du système d'authentification
+*/
+app.controller('HomeController',['$scope', '$http','$location','$rootScope', function($scope, $http,$location,$rootScope) {
+  
+
+  
+  
+}]);

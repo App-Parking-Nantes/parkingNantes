cc6ecee6f7f38ea81815513eef17f77160383a84
BaptisteDixneuf <baptiste.dixneuf@gmail.com>
2015-03-29 21:42:37 +0200

    Localisation + horaires

--- public_html/js/controller/HomeController.js
+++ public_html/js/controller/HomeController.js
@@ -4,30 +4,33 @@
 app.controller('HomeController', ['$scope', '$http', '$location', '$rootScope', '$q', function ($scope, $http, $location, $rootScope, $q) {
 
         $scope.parkings = [];
-        $scope.localisation = [];
+        $scope.horaires = [];
+        $scope.localisations = [];
+
+        /**
+         * Problème liée au cross-domain
+         * exécution de code JavaScript dans un navigateur répond å certaines
+         * contraintes de sécurité, tel que l'exécution "cross-domain", 
+         * en particulier pour les requêtes Ajax.
+         * Comprendre que pour une page s'exécutant sur un domaine "x",
+         * le navigateur ne va pas forcément exécuter directement 
+         * le code chargé depuis un domaine "y".
+         */
+        // http://stackoverflow.com/questions/22546177/cross-domain-http-request-angularjs --> CROSS DOMAIN
+        // http://stackoverflow.com/questions/26262235/jsonp-returning-uncaught-syntaxerror-unexpected-token-angularjs-routingnu -> json callback
+        // http://stackoverflow.com/questions/16661032/http-get-is-not-allowed-by-access-control-allow-origin-but-ajax-is -> allow Origin
+        // http://openclassrooms.com/forum/sujet/jquery-getjson-sans-parametres-75069 
+        // http://data.nantes.fr/forum/?tx_mmforum_pi1%5Baction%5D=list_post&tx_mmforum_pi1%5Btid%5D=123
+
+
 
         $scope.dataParkings = function () {
 
             //On récupère les données
             var deferred = $q.defer();
 
-            /**
-             * Problème liée au cross-domain
-             * exécution de code JavaScript dans un navigateur répond å certaines
-             * contraintes de sécurité, tel que l'exécution "cross-domain", 
-             * en particulier pour les requêtes Ajax.
-             * Comprendre que pour une page s'exécutant sur un domaine "x",
-             * le navigateur ne va pas forcément exécuter directement 
-             * le code chargé depuis un domaine "y".
-             */
-            // http://stackoverflow.com/questions/22546177/cross-domain-http-request-angularjs --> CROSS DOMAIN
-            // http://stackoverflow.com/questions/26262235/jsonp-returning-uncaught-syntaxerror-unexpected-token-angularjs-routingnu -> json callback
-            // http://stackoverflow.com/questions/16661032/http-get-is-not-allowed-by-access-control-allow-origin-but-ajax-is -> allow Origin
-            // http://openclassrooms.com/forum/sujet/jquery-getjson-sans-parametres-75069 
-            // http://data.nantes.fr/forum/?tx_mmforum_pi1%5Baction%5D=list_post&tx_mmforum_pi1%5Btid%5D=123
-            
 
-            $http.get("http://baptistedixneuf.fr/parking/parking.php").
+            $http.get("http://baptistedixneuf.fr/parking/parkings.php").
                     success(function (data, status) {
                         deferred.resolve(data);
                     }).
@@ -45,6 +48,28 @@ app.controller('HomeController', ['$scope', '$http', '$location', '$rootScope', 
         };
 
 
+        $scope.dataHoraire = function () {
+            //On récupère les données
+            var deferred = $q.defer();
+
+            $http.get("http://baptistedixneuf.fr/parking/horaireParkings.php").
+                    success(function (data, status) {
+                        deferred.resolve(data);
+                    }).
+                    error(function (data, status) {
+                        deferred.reject('Erreur requete ajax: Connexion serveur impossible');
+                    });
+
+            data = deferred.promise;
+
+            $scope.horaires = data.then(function (data) {
+                $scope.horaires = data.data;
+            }, function (msg) {
+                alert(msg);
+            });
+        };
+        
+        
         $scope.dataLocalisation = function () {
             //On récupère les données
             var deferred = $q.defer();
@@ -59,15 +84,16 @@ app.controller('HomeController', ['$scope', '$http', '$location', '$rootScope', 
 
             data = deferred.promise;
 
-            $scope.localisation = data.then(function (data) {
-                $scope.localisation = data;
+            $scope.localisations = data.then(function (data) {
+                $scope.localisations = data.data;
             }, function (msg) {
                 alert(msg);
             });
         };
-                
+
 
         $scope.dataParkings();
+        $scope.dataHoraire();
         $scope.dataLocalisation();
 
     }]);
--- public_html/php/horaireParkings.php
+++ public_html/php/horaireParkings.php
@@ -1,6 +1,6 @@
 <?php
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-$json = file_get_contents("http://data.nantes.fr/api/publication/24440040400129_NM_NM_00022/LOC_EQUIPUB_MOBILITE_NM_STBL/content/?format=json");
+$json = file_get_contents("http://data.nantes.fr/api/publication/24440040400129_NM_NM_00010/LISTE_HORAIRES_PKGS_PUB_NM_STBL/content/?format=json");
 echo $json;
 ?>
